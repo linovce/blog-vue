34479ba28f4f0900283bc64ad586415f42dbf702
linovce <1076150551@qq.com>
2021-04-12 16:47:18 +0800

    实现分页显示

--- src/views/ArticleManage.vue
+++ src/views/ArticleManage.vue
@@ -1,97 +1,119 @@
 <template>
-  <el-table :data="tableData" border style="width: 100%">
-    <el-table-column prop="articleId" label="文章编号"v-if="false"> </el-table-column>
-    <el-table-column width="320" prop="articleName" label="标题" >
-      <template slot-scope="scope">
-        <el-tag v-if="scope.row.original" type="success" effect="dark" size="mini">
-          <span>原创</span>
-        </el-tag>
-        <el-tag v-else effect="dark" size="mini">
-          <span>转载</span>
-        </el-tag>
-        <span style="margin-left: 10px">{{ scope.row.articleName }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column width="80" prop="views" label="浏览"> </el-table-column>
-    <el-table-column width="80" prop="appreciate" label="点赞" > </el-table-column>
-    <el-table-column label="评论开启">
-      <template slot-scope="scope">
-        <el-switch
-                v-model="scope.row.comments"
-                active-text="开启"
-                @change="updateComment(scope.row)">
-        </el-switch>
-      </template>
-    </el-table-column>
-    <el-table-column label="版权显示">
-      <template slot-scope="scope">
-        <el-switch
-                v-model="scope.row.copyright"
-                active-text="开启"
-                @change="updateCopyright(scope.row)">
-        </el-switch>
-      </template>
-    </el-table-column>
-    <el-table-column label="发布">
-      <template slot-scope="scope">
-        <el-switch
-                v-model="scope.row.publish"
-                active-text="发布"
-                @change="updatePublish(scope.row)">
-        </el-switch>
-      </template>
-    </el-table-column>
-    <el-table-column prop="createDate" label="发布时间" > </el-table-column>
-    <el-table-column prop="doneDate" label="操作时间" > </el-table-column>
-    <el-table-column label="操作" width="180">
-      <template slot-scope="scope">
-        <el-button type="primary" icon="el-icon-edit" circle size="mini"></el-button>
-        <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
-        <el-button type="success" icon="el-icon-view" circle size="mini"></el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="articleId" label="文章编号"v-if="false"> </el-table-column>
+      <el-table-column width="320" prop="articleName" label="标题" >
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.original" type="success" effect="dark" size="mini">
+            <span>原创</span>
+          </el-tag>
+          <el-tag v-else effect="dark" size="mini">
+            <span>转载</span>
+          </el-tag>
+          <span style="margin-left: 10px">{{ scope.row.articleName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column width="80" prop="views" label="浏览"> </el-table-column>
+      <el-table-column width="80" prop="appreciate" label="点赞" > </el-table-column>
+      <el-table-column label="评论开启">
+        <template slot-scope="scope">
+          <el-switch
+                  v-model="scope.row.comments"
+                  active-text="开启"
+                  @change="updateComment(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="版权显示">
+        <template slot-scope="scope">
+          <el-switch
+                  v-model="scope.row.copyright"
+                  active-text="开启"
+                  @change="updateCopyright(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="发布">
+        <template slot-scope="scope">
+          <el-switch
+                  v-model="scope.row.publish"
+                  active-text="发布"
+                  @change="updatePublish(scope.row)">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column prop="createDate" label="发布时间" > </el-table-column>
+      <el-table-column prop="doneDate" label="操作时间" > </el-table-column>
+      <el-table-column label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" circle size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle size="mini"></el-button>
+          <el-button type="success" icon="el-icon-view" circle size="mini"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="float: right;padding-top: 10px">
+      <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="page.currentPage"
+              :page-sizes="page.pageSizes"
+              :page-size="page.pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="page.total">
+      </el-pagination>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   name: "ArticleManage",
   data() {
     return {
-      tableData: [{
-        articleId:1,
-        articleName:'测试1',
-        views:100,
-        appreciate:100,
-        createDate:'2021-04-09',
-        doneDate:'2021-04-09',
-        type: 'success',
-        type_text:true
-      },
-        {
-          articleId:1,
-          articleName:'测试1',
-          views:100,
-          appreciate:100,
-          createDate:'2021-04-09',
-          doneDate:'2021-04-09',
-          type: '',
-          type_text:false
-        }],
+      tableData: [],
       isComments: true,
-      value1: true
+      value1: true,
+      page: {
+        total:100,
+        pageSize:5,
+        pageSizes:[5,10,20,30,40,50],
+        currentPage:1
+      }
     }
 
   },
   methods:{
+    handleSizeChange(val){
+      var __this = this;
+      __this.page.pageSize=val;
+      console.log(__this.page.pageSize)
+      __this.selectAll();
+    },
+    handleCurrentChange(val){
+      var __this = this;
+      __this.page.currentPage=val;
+      console.log(__this.page.currentPage)
+      __this.selectAll();
+    },
     selectAll(){
       var __this = this;
+      const page = {
+        pageNum:__this.page.currentPage,
+        pageSize:__this.page.pageSize
+      }
+      console.log(page)
       this.axios({
         method:'get',
-        url:'http://127.0.0.1:8081/listArticle',
+        url:'http://127.0.0.1:8081/pagingArticle',
+        params:{
+          pageNum:__this.page.currentPage,
+          pageSize:__this.page.pageSize
+        }
       })
         .then(function (response) {
           console.log(response.data);
-          __this.tableData=response.data
+          __this.tableData = response.data.list;
+          __this.page.total = response.data.total;
         })
     },
     updateComment(row){
